4f35d8345f7d2bd5c1e28ffd16c7b75db309a0e2
Erik S. <eriksom764@gmail.com>
2026-08-11 10:34:38 +0200

    added more style in add task to board

--- scripts/board.js
+++ scripts/board.js
@@ -16,6 +16,31 @@ function stopPropagation(event) {
 }
 
 // Funktionen, die nur für board gedacht sind
+function removeInput() {
+  let inputRef = document.getElementById("subtaskInput");
+  inputRef.value = "";
+}
+
+function addInput() {
+  let inputRef = document.getElementById("subtaskInput");
+  let subtasksRef = document.getElementById("subtasks");
+  if (inputRef.value != "") {
+    subtasksRef.innerHTML += `
+      <li>
+        <p class="subtask_text">${inputRef.value}</p>
+        <button class="subtask_icon">
+          <img src="../assets/img/summary/penValidate.svg" alt="Edit subtask" />
+        </button>
+        <div class="subtask_middle"></div>
+        <button class="subtask_icon">
+          <img src="../assets/img/general/delete.svg" alt="Delete subtask" />
+        </button>
+      </li>
+    `;
+  }
+  removeInput();
+}
+
 function toggleCustomDropdown(id) {
   const dropdown = document.getElementById(id);
   if (!dropdown) return;
