bfde4710108dfa999d9ab854f4b4f1143f0b75c1
Erik S. <eriksom764@gmail.com>
2026-09-17 11:44:19 +0200

    added initials to contacts in add task

--- scripts/addTaskTemplate.js
+++ scripts/addTaskTemplate.js
@@ -48,12 +48,11 @@ function recreateSubtaskTemplate(subtask, index) {
 }
 
 async function contactsTemplate(contact) {
-  // ${await getInitials(contact.name)}
   // <input type="checkbox" class="subtask_checkbox" data-subtask-index="${index}" ${checked ? "checked" : ""} />
   return `
     <button class="select_contacts_option assign" type="button" data-value="Contact">
       <div class="assign">
-        <div class="contact_color" style="background-color: var(${contact.color});">A</div>   
+        <div class="contact_color" style="background-color: var(${contact.color});">${await getInitials(contact.name)}</div>   
         <p class="select_areas_value">${contact.name}</p>
       </div>
         <input type="checkbox" name="" id="">
